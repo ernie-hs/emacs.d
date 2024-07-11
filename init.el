5caf94164df9eac1bc1f988b2272982eefd694e4
ernie <ernie_hs@hotmail.com>
2024-07-11 09:45:06 +0100

    tinkering

--- init.el
+++ init.el
@@ -30,3 +30,45 @@
 (setq use-package-always-ensure t)
 
 ;; packages
+
+(use-package zenburn-theme
+  :ensure t
+  :init (load-theme 'zenburn t))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom (doom-modeline-height 15))
+
+(use-package parinfer-rust-mode
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+(use-package clojure-mode
+  :ensure t
+  :init (add-hook 'clojure-mode-hook 'parinfer-rust-mode)
+        (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+	(add-hook 'cider-repl-mode-hook 'parinfer-rust-mode))
+
+(use-package clojure-mode-extra-font-locking
+  :ensure t
+  :init (require 'clojure-mode-extra-font-locking))
+
+(use-package cider
+  :ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rainbow-delimiters cider clojure-mode-extra-font-locking clojure-mode parinfer-rust-mode doom-modeline zenburn-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
